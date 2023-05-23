e831d7e20f69a77eaf356d43ac26f4d95485787f
AugustoGa <augustogallo98@gmail.com>
2023-05-23 00:11:38 -0300

    arreglos

--- PreEntrega1/src/main/java/com/augustogallo/PreEntrega1/repository/ClientRepository.java
+++ PreEntrega1/src/main/java/com/augustogallo/PreEntrega1/repository/ClientRepository.java
@@ -1,2 +1,7 @@
-package com.augustogallo.PreEntrega1.repository;public interface ClientRepository {
+package com.augustogallo.PreEntrega1.repository;
+
+import com.augustogallo.PreEntrega1.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client , Integer> {
 }
