01789c966161fc6d18f6dfc5221fc63b184a6407
AugustoGa <augustogallo98@gmail.com>
2023-05-22 23:09:06 -0300

    entrega

---
+++ PreEntrega1/src/main/java/com/augustogallo/PreEntrega1/model/Client.java
@@ -0,0 +1,69 @@
+package model;
+
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import javax.naming.AuthenticationException;
+
+@Entity
+@Table(name="client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String lastname;
+
+    private String docnumber;
+
+    //Getter and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDocnumber() {
+        return docnumber;
+    }
+
+    public void setDocnumber(String docnumber) {
+        this.docnumber = docnumber;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", docnumber='" + docnumber + '\'' +
+                '}';
+    }
+
+
+}
---
+++ PreEntrega1/src/main/java/com/augustogallo/PreEntrega1/model/Product.java
@@ -0,0 +1,79 @@
+package model;
+
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String description;
+
+    private String code;
+
+    private int stock;
+
+    private double price;
+
+
+    //Getters and Setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", code='" + code + '\'' +
+                ", stock=" + stock +
+                ", price=" + price +
+                '}';
+    }
+}
