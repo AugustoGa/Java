df95dbfb0e180206d91902893f34ea359b60f764
AugustoGa <augustogallo98@gmail.com>
2023-05-25 23:42:33 -0300

    entrega

---
+++ ClientApiRest/src/main/java/edu/coderhouse/ClientApiRest/ClientApiRest.java
@@ -0,0 +1,20 @@
+package edu.coderhouse.ClientApiRestLetticugna;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApiRest implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApiRest.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server listening. Access H2 on: http://localhost:8888/h2-console");
+
+	}
+}
+
