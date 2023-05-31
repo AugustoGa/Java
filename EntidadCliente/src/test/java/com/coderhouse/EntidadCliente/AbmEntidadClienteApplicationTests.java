02a527208d58d518c14135a4f472c9f474bc7468
AugustoGa <augustogallo98@gmail.com>
2023-05-30 23:05:04 -0300

    entrega

---
+++ EntidadCliente/src/test/java/com/coderhouse/EntidadCliente/AbmEntidadClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.EntidadCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmEntidadClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
